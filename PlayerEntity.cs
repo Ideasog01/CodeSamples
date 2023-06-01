90199e0a90c1ca2edd1ff959f6e5ae030482a42f
Ideasog01 <92227089+Ideasog01@users.noreply.github.com>
2023-06-01 21:24:12 +0100

    Add files via upload

---
+++ PlayerEntity.cs
@@ -0,0 +1,234 @@
+using UnityEngine;
+
+public class PlayerEntity : BaseEntity
+{
+    public static bool plasmaActive;
+
+    public static bool laserActive;
+
+    public static float plasmaCharge;
+
+    public static float powerChargeAmount = 100;
+
+    [Header("Plasma Cannon")]
+
+    public Transform plasmaProjectilePrefab;
+
+    [SerializeField]
+    private Transform plasmaSpawnPos;
+
+    [SerializeField]
+    private float plasmaChargeRate;
+
+    [SerializeField]
+    private float plasmaChargeCost;
+
+    [SerializeField]
+    private float plasmaChargeTime;
+
+    [SerializeField]
+    private VisualEffect plasmaChargeEffect;
+
+    [SerializeField]
+    private VisualEffect plasmaFireEffect;
+
+    [SerializeField]
+    private Sound plasmaChargeSound;
+
+    [SerializeField]
+    private Sound plasmaFireSound;
+
+    [Header("Laser")]
+
+    [SerializeField]
+    private Transform laserOrigin;
+
+    [SerializeField]
+    private LineRenderer laserLineRenderer;
+
+    [SerializeField]
+    private Transform laserEnd;
+
+    [SerializeField]
+    private GameObject laserEndVFX;
+
+    [SerializeField]
+    private float laserRotationSpeed;
+
+    [SerializeField]
+    private Sound laserSound;
+
+    [Header("General")]
+
+    [SerializeField]
+    private float powerRechargeRate;
+
+    private bool _isPlasmaCharging;
+
+    private bool _isLaserFiring;
+
+    #region HandCannonSettings
+
+    [Header("Hand Cannon Settings")]
+
+    [SerializeField]
+    private float[] projectileBlastRadius;
+
+    [SerializeField]
+    private float[] projectileVelocity;
+
+    [SerializeField]
+    private float[] powerEffectiveness;
+
+    [SerializeField]
+    private float[] chargeTime;
+
+    [SerializeField]
+    private int[] damageOutput;
+
+    public float[] ProjectileBlastRadius
+    {
+        get { return projectileBlastRadius; }
+    }
+
+    public float[] ProjectileVelocity
+    {
+        get { return projectileVelocity; }
+    }
+
+    public int[] DamageOutput
+    {
+        get { return damageOutput; }
+    }
+
+    public float PlasmaChargeTime
+    {
+        get { return plasmaChargeTime; }
+    }
+
+    #endregion
+
+    private void Awake()
+    {
+        powerChargeAmount = 100;
+    }
+
+    private void Update()
+    {
+        if(_isPlasmaCharging)
+        {
+            ChargePlasmaCannon();
+        }
+
+        if(_isLaserFiring)
+        {
+            laserEnd.Rotate(new Vector3(0, 0, 1) * laserRotationSpeed); //Visual
+        }
+
+        if(powerChargeAmount < 100) //Recharge power (Power allows the player to use their main equipment)
+        {
+            powerChargeAmount += Time.deltaTime * powerRechargeRate;
+        }
+    }
+
+    public void FireCannon(bool release)
+    {
+        if (!GameManager.gameInProgress && !release)
+        {
+            return;
+        }
+
+        if (plasmaActive)
+        {
+            if(release)
+            {
+                if(plasmaCharge >= plasmaChargeTime) //Is plasma cannon ready to fire?
+                {
+                    GameManager.spawnManager.SpawnPlayerProjectile(plasmaProjectilePrefab, plasmaSpawnPos.position, plasmaSpawnPos.rotation);
+
+                    //Visuals
+                    GameManager.visualEffectManager.StartVFX(plasmaFireEffect);
+                    GameManager.audioManager.PlaySound(plasmaFireSound);
+                    Debug.Log("Cannon Fired");
+
+                }
+
+                //Reset plasma cannon
+                plasmaCharge = 0;
+                _isPlasmaCharging = false;
+
+                GameManager.visualEffectManager.StopVFX(plasmaChargeEffect);
+                GameManager.audioManager.StopSound(plasmaChargeSound);
+
+                GameManager.guiManager.DisplayPlasmaCharge();
+                GameManager.guiManager.TogglePlasmaCharge(false);
+            }
+            else //Start charging the plasma cannon
+            {
+                plasmaChargeTime = chargeTime[GameManager.mainLoadout.GeneralSettingsValueArray[3]];
+                plasmaChargeCost = powerEffectiveness[GameManager.mainLoadout.GeneralSettingsValueArray[2]];
+
+                GameManager.visualEffectManager.StartVFX(plasmaChargeEffect);
+                GameManager.audioManager.PlaySound(plasmaChargeSound);
+
+                _isPlasmaCharging = true;
+                GameManager.guiManager.TogglePlasmaCharge(true);
+            }
+        }
+    }
+
+    public void FireLaser(bool active)
+    {
+        if(laserActive)
+        {
+            _isLaserFiring = active;
+            laserLineRenderer.enabled = active;
+            
+            if(active)
+            {
+                GameManager.audioManager.PlaySound(laserSound);
+            }
+            else
+            {
+                GameManager.audioManager.StopSound(laserSound);
+                laserEndVFX.SetActive(false); //As the laser is now disabled, and laser end vfx is activated when the laser collides with an object and deactivated when not.
+            }
+        }
+    }
+
+    private void ChargePlasmaCannon()
+    {
+        if (plasmaCharge < plasmaChargeTime) //Increment the plasma charge to the charge time
+        {
+            plasmaCharge += Time.deltaTime * plasmaChargeRate;
+
+            float chargePowerMod = 1; //Multiply by charge time, as the time allowed for power to decrease is less
+
+            if (GameManager.mainLoadout.GeneralSettingsValueArray[3] > 0) //So the multiplier is not 0, resulting in an incorrect result
+            {
+                chargePowerMod = GameManager.mainLoadout.GeneralSettingsValueArray[3];
+            }
+
+            powerChargeAmount -= Time.deltaTime * plasmaChargeCost * chargePowerMod; 
+
+            if (powerChargeAmount <= 0)
+            {
+                Debug.Log("Cannon ran out of charge");
+                plasmaCharge = 0;
+                _isPlasmaCharging = false;
+
+                GameManager.guiManager.DisplayPlasmaCharge();
+
+                GameManager.guiManager.TogglePlasmaCharge(false);
+                GameManager.visualEffectManager.StopVFX(plasmaChargeEffect);
+                GameManager.audioManager.StopSound(plasmaChargeSound);
+            }
+        }
+        else //Once the charge reaches the charge time, fire a projectile and reset
+        {
+            FireCannon(true);
+        }
+
+        GameManager.guiManager.DisplayPlasmaCharge(); //Continue to display the charge when trigger is pressed and is charging
+    }
+}
---
+++ SpawnManager.cs
@@ -0,0 +1,196 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static int activeHostiles;
+
+    [Header("Spawn Settings")]
+
+    public Transform[] spawnArray;
+
+    public Animator[] doorAnimators;
+
+    [Header("Other")]
+
+    [SerializeField]
+    private Transform[] enemyPrefabs;
+
+    [SerializeField]
+    private Transform projectileParent;
+
+    [SerializeField]
+    private Transform enemyParent;
+
+    [SerializeField]
+    private bool activateGameMode;
+
+    public List<EnemyEntity> enemyList = new List<EnemyEntity>();
+
+    private List<PlayerProjectileController> _activePlayerProjectiles = new List<PlayerProjectileController>();
+
+    private List<ProjectileController> _activeProjectiles = new List<ProjectileController>();
+
+    private List<ProjectileController> _activeTurretProjectiles = new List<ProjectileController>();
+
+    private List<ProjectileController> _seekerProjectiles = new List<ProjectileController>();
+
+    private void Start()
+    {
+        activeHostiles = 0;
+
+        if(activateGameMode)
+        {
+            SpawnWaveManager();
+        }
+    }
+
+    public void SpawnWaveManager()
+    {
+        GameManager.waveManager = Instantiate(GameManager.gameSettings.SpawnPrefab.GetComponent<WaveManager>(), Vector3.zero, Quaternion.identity); //Spawn the WaveManager dependent on the selected game settings (difficulty level)
+    }
+
+    public PlayerProjectileController SpawnPlayerProjectile(Transform prefab, Vector3 position, Quaternion rotation)
+    {
+        PlayerProjectileController projectileToUse = null;
+
+        foreach (PlayerProjectileController projectile in _activePlayerProjectiles) //Reuse projectile (avoids uneccessary use of memory)
+        {
+            if (!projectile.gameObject.activeSelf)
+            {
+                projectile.gameObject.SetActive(true);
+                projectileToUse = projectile;
+                break;
+            }
+        }
+
+        if (projectileToUse == null) //Projectile not found, create a new one
+        {
+            projectileToUse = Instantiate(prefab.GetComponent<PlayerProjectileController>(), position, rotation);
+            projectileToUse.transform.parent = projectileParent;
+            _activePlayerProjectiles.Add(projectileToUse);
+        }
+
+        projectileToUse.InitialiseProjectile(position, rotation); //Reset the projectile
+
+        return projectileToUse;
+    }
+
+    public void SpawnProjectile(Transform prefab, Vector3 position, Quaternion rotation)
+    {
+        ProjectileController projectileToUse = null;
+
+        foreach (ProjectileController projectile in _activeProjectiles) //Reuse projectile (avoids uneccessary use of memory)
+        {
+            if(!projectile.gameObject.activeSelf)
+            {
+                projectile.gameObject.SetActive(true);
+                projectileToUse = projectile;
+                break;
+            }
+        }
+
+        if(projectileToUse == null) //Projectile not found, create a new one
+        {
+            projectileToUse = Instantiate(prefab.GetComponent<ProjectileController>(), position, rotation);
+            projectileToUse.transform.parent = projectileParent;
+            _activeProjectiles.Add(projectileToUse);
+        }
+
+        projectileToUse.InitialiseProjectile(position, rotation); //Reset the projectile
+    }
+
+
+    public void SpawnTurretProjectile(Transform prefab, Vector3 position, Quaternion rotation)
+    {
+        ProjectileController projectileToUse = null;
+
+        foreach (ProjectileController projectile in _activeTurretProjectiles) //Reuse projectile (avoids uneccessary use of memory)
+        {
+            if (!projectile.gameObject.activeSelf)
+            {
+                projectile.gameObject.SetActive(true);
+                projectileToUse = projectile;
+                break;
+            }
+        }
+
+        if (projectileToUse == null) //Projectile not found, create a new one
+        {
+            projectileToUse = Instantiate(prefab.GetComponent<ProjectileController>(), position, rotation);
+            projectileToUse.transform.parent = projectileParent;
+            _activeTurretProjectiles.Add(projectileToUse);
+        }
+
+        projectileToUse.InitialiseProjectile(position, rotation); //Reset the projectile
+    }
+
+    public ProjectileController SpawnSeekerProjectile(Transform prefab, Vector3 position, Quaternion rotation)
+    {
+        ProjectileController projectileToUse = null;
+
+        foreach (ProjectileController projectile in _seekerProjectiles) //Reuse projectile (avoids uneccessary use of memory)
+        {
+            if (!projectile.gameObject.activeSelf)
+            {
+                projectile.gameObject.SetActive(true);
+                projectileToUse = projectile;
+                break;
+            }
+        }
+
+        if (projectileToUse == null) //Projectile not found, create a new one
+        {
+            projectileToUse = Instantiate(prefab.GetComponent<ProjectileController>(), position, rotation);
+            projectileToUse.transform.parent = projectileParent;
+            _seekerProjectiles.Add(projectileToUse);
+        }
+
+        projectileToUse.InitialiseProjectile(position, rotation); //Reset the projectile
+
+        return projectileToUse;
+    }
+
+    public void SpawnEnemy(SpawnSettings spawnSettings)
+    {
+        EnemyEntity enemyEntity = null;
+
+        foreach(EnemyEntity enemy in enemyList)
+        {
+            if(!enemy.gameObject.activeSelf)
+            {
+                if(enemy.EnemyTypeRef == spawnSettings.EnemyTypeToSpawn) //Is the same enemy, and is inactive, then use this enemy object for the new enemy
+                {
+                    enemy.gameObject.SetActive(true);
+                    enemyEntity = enemy;
+                    break;
+                }
+            }
+        }
+
+        //Spawn settings
+        int spawnIndex = spawnSettings.SpawnLocationIndex;
+        Transform spawnPos = spawnArray[spawnIndex];
+        Transform initialTargetPos = spawnPos.GetChild(0);
+        Animator door = doorAnimators[spawnIndex];
+
+
+        if (enemyEntity == null) //An inactive enemy was not found, so instantiate a new enemy object
+        {
+            enemyEntity = Instantiate(enemyPrefabs[(int)spawnSettings.EnemyTypeToSpawn].GetComponent<EnemyEntity>(), spawnPos.position, spawnPos.rotation);
+            enemyEntity.transform.parent = enemyParent;
+            enemyList.Add(enemyEntity);
+        }
+
+        enemyEntity.ResetEnemy(spawnPos.position); //Resets the enemy properties to their default state
+        enemyEntity.InitialTargetLocation = initialTargetPos;
+
+        door.SetBool("open", true); //Opens the door. The door will be open until it is closed by this enemy when it reaches the inital target location
+        enemyEntity.DoorAnimator = door; //Assigns the door animator to the enemy for later
+
+        activeHostiles++;
+    }
+
+}
